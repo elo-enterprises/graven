912d7e5a74ad2f1aae632c41a838ab9197f521f2
mvr <matt@example.com>
2023-07-06 14:51:10 -0700

    bugfix after leaving versioneer

--- src/graven/__init__.py
+++ src/graven/__init__.py
@@ -1,3 +1,9 @@
-
-from . import _version
-__version__ = _version.get_versions()['version']
+"""graven
+"""
+# NB: this should have been set by CI immediately
+# before pypi-upload.  but the `except` below might
+# be triggered by local development.
+try:
+    from ._version import __version__  # noqa
+except ImportError:
+    __version__ = "0.0.0+local"
